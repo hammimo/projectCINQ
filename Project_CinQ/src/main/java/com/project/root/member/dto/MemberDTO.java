6ec60f8ee0cad707204257aa3c37b007de8c5ede
hm676 <hm676@DESKTOP-8PH8QOJ>
2022-10-19 16:20:08 +0900

    memberDTO 생성입니다

---
+++ Project_CinQ/src/main/java/com/project/root/member/dto/MemberDTO.java
@@ -0,0 +1,58 @@
+package com.project.root.member.dto;
+
+public class MemberDTO {
+	
+	private String name;
+	private String id;
+	private String pw;
+	private String pid;
+	private int tel;
+	private String gender;
+	private String nickname;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getPid() {
+		return pid;
+	}
+	public void setPid(String pid) {
+		this.pid = pid;
+	}
+	public int getTel() {
+		return tel;
+	}
+	public void setTel(int tel) {
+		this.tel = tel;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
+	
+}
